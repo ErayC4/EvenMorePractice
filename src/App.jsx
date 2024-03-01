80915d065f0c4f3537b0495120e1129461c549ec
ErayC4 <eray04.coskun@gmail.com>
2024-03-01 19:17:39 +0100

    experimenting with parallax and spring

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import React from 'react'
-import './index.css'
+import React from "react";
+import "./index.css";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 function App() {
-
   return (
-    <>
-      hi
-    </>
-  )
+    <div>
+      <Parallax
+        className="text-6xl text-white bg-gray-900 h-screen w-full"
+        pages={4}
+      >
+        <div className="flex justify-between">
+          <ParallaxLayer speed={1}>
+            <p>Whale Cum</p>
+          </ParallaxLayer>
+
+          <ParallaxLayer
+            speed={0.5}
+            offset={1}
+            factor={2}
+            className="text-6xl text-white bg-cover h-screen"
+            style={{ backgroundImage: 'url("/images/wolken.jpg")' }}
+          >
+            <p>Lorem in mein Ipsum</p>
+          </ParallaxLayer>
+        </div>
+      </Parallax>
+    </div>
+  );
 }
 
-export default App
+export default App;
